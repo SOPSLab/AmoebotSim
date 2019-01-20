3a1ed804245ad08ef386d1baf9faf4705ffe32ce
Josh Daymude <joshdaymude@gmail.com>
2019-01-20 13:35:19 -0700

    + Added main LICENSE file and full copyright notice.
    + Added an abbreviated copyright notice to DiscoDemo.

--- alg/demo/discodemo.cpp
+++ alg/demo/discodemo.cpp
@@ -1,6 +1,6 @@
-// Copyright (C) 2018 Robert Gmyr, Joshua J. Daymude, and Kristian Hinnenthal
-// The full GNU GPLv3 can be found in the LICENSE file, and the full copyright
-// notice can be found at the top of main/main.cpp.
+/* Copyright (C) 2019 Joshua J. Daymude, Robert Gmyr, and Kristian Hinnenthal.
+ * The full GNU GPLv3 can be found in the LICENSE file, and the full copyright
+ * notice can be found at the top of main/main.cpp. */
 
 #include "alg/demo/discodemo.h"
 
