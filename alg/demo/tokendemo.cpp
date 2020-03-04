4efa55e67ff01b1d6d207098ca9d1f5b1fed2c86
Ziad Abdelkarim <zabdelka@asu.edu>
2020-03-03 21:52:51 -0700

    Modified Token Demo
    + State field and labelOfFirstNbrInState(...) in tokenDemo class
    + Tokens disappear after being passed x times.

--- alg/demo/tokendemo.cpp
+++ alg/demo/tokendemo.cpp
@@ -8,22 +8,15 @@
 
 TokenDemoParticle::TokenDemoParticle(const Node head, const int globalTailDir,
                                      const int orientation,
-                                     AmoebotSystem& system, State state,
-                                     const QString mode)
-  : ShapeFormationParticle(head, globalTailDir, orientation, system, state,
-                           mode) {
+                                     AmoebotSystem& system, State state)
+  :  AmoebotParticle(head, globalTailDir, orientation, system),
+    state(state) {
   // Initialize the seed particle to hold three red tokens and two blue.
   if (state == State::Seed) {
-    putToken(std::make_shared<RedToken>());
-    putToken(std::make_shared<RedToken>());
-    putToken(std::make_shared<RedToken>());
-    putToken(std::make_shared<RedToken>());
-    putToken(std::make_shared<RedToken>());
-    putToken(std::make_shared<BlueToken>());
-    putToken(std::make_shared<BlueToken>());
-    putToken(std::make_shared<BlueToken>());
-    putToken(std::make_shared<BlueToken>());
-    putToken(std::make_shared<BlueToken>());
+    for (int i = 0; i < 5; i++) {
+      putToken(std::make_shared<RedToken>());
+      putToken(std::make_shared<BlueToken>());
+    }
   }
 }
 
@@ -35,7 +28,17 @@ void TokenDemoParticle::activate() {
     if (isContracted() && (state == State::Seed || state == State::Finish)) {
       int lbl = labelOfFirstNbrInState({State::Seed, State::Finish}, randDir());
       if (lbl != -1) {
-        nbrAtLabel(lbl).putToken(takeToken<Token>());
+        if(hasToken<BlueToken>()) {
+          std::shared_ptr<BlueToken> t = takeToken<BlueToken>();
+          t->lifeCycle--;
+          if(t->lifeCycle != 0)
+            nbrAtLabel(lbl).putToken(t);
+        } else {
+          std::shared_ptr<RedToken> t = takeToken<RedToken>();
+          t->lifeCycle--;
+          if(t->lifeCycle != 0)
+            nbrAtLabel(lbl).putToken(t);
+        }
       }
     }
   }
@@ -54,7 +57,7 @@ int TokenDemoParticle::headMarkColor() const {
 }
 
 QString TokenDemoParticle::inspectionText() const {
-  QString text = ShapeFormationParticle::inspectionText();
+  QString text = AmoebotParticle::inspectionText();
   text += "numRedTokens: " + QString::number(countTokens<RedToken>());
   text += "\n";
   text += "numBlueTokens: " + QString::number(countTokens<BlueToken>());
@@ -66,25 +69,39 @@ TokenDemoParticle& TokenDemoParticle::nbrAtLabel(int label) const {
   return AmoebotParticle::nbrAtLabel<TokenDemoParticle>(label);
 }
 
+int TokenDemoParticle::labelOfFirstNbrInState(
+    std::initializer_list<State> states, int startLabel) const {
+  auto prop = [&](const TokenDemoParticle& p) {
+    for (auto state : states) {
+      if (p.state == state) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  return labelOfFirstNbrWithProperty<TokenDemoParticle>(prop, startLabel);
+}
 TokenDemoSystem::TokenDemoSystem(int numParticles) {
   Q_ASSERT(numParticles > 0);
 
   // Insert the seed at (0, 0).
   insert(new TokenDemoParticle(Node(0, 0), -1, randDir(), *this,
-                               ShapeFormationParticle::State::Seed, "l"));
+                               TokenDemoParticle::State::Seed));
 
   int sideLen = static_cast<int>(std::round(1.4 * std::sqrt(numParticles)));
   Node boundNode = Node(0, 0);
   for (int dir = 0; dir < 6; ++dir) {
     for (int i = 0; i < sideLen; ++i) {
 
+      // Reaching the last node position of the hexagon
       if(dir == 5 && i == sideLen - 1) {
         return;
       }
 
       boundNode = boundNode.nodeInDir(dir);
       insert(new TokenDemoParticle(boundNode, -1, randDir(), *this,
-                                   ShapeFormationParticle::State::Finish, "l"));
+                                   TokenDemoParticle::State::Finish));
     }
   }
 }
