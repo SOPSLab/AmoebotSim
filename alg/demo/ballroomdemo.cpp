111c30fe3ca65c05f1b512eefa0bd552367e9510
Joseph Briones <jbriones07@gmail.com>
2020-03-31 14:37:06 -0700

    Updated system to be enclosed in a box
    + spawned a rhombic box around the system to make it easier to understand visually.

--- alg/demo/ballroomdemo.cpp
+++ alg/demo/ballroomdemo.cpp
@@ -97,33 +97,61 @@ BallroomDemoParticle& BallroomDemoParticle::nbrAtLabel(int label) const {
 }
 
 BallroomDemoSystem::BallroomDemoSystem(unsigned int numParticles) {
-  // Insert the leader at (0,0) and the follower at (-1,0).
+
+  // The Ballroom system is enclosed in a rhombus with side length
+  // of 2 * numParticles.
+  Node boundNode(numParticles, numParticles);
+  int direcitons[4] = {3, 4, 0, 1};
+  for (int j = 0; j < 4; ++j) {
+    int dir = direcitons[j];
+    for (int i = 0; i < 2*numParticles; ++i) {
+      insert(new Object(boundNode));
+      boundNode = boundNode.nodeInDir(dir);
+    }
+  }
+
   std::set<Node> occupied;
-  while (occupied.size() < numParticles) {
-    // First, choose an x and y position at random from the (i) and (ii) bounds.
-    int x = randInt(-numParticles, numParticles);
-    int y = randInt(-numParticles, numParticles);
-    Node node(x, y);
+  while ((occupied.size()/3) < numParticles) {
+    // First, choose an x and y position at random within the rhombus.
+    int x = randInt(-(2*numParticles/3), (2*numParticles/3));
+    int y = randInt(-(2*numParticles/3), (2*numParticles/3));
+
+    // Then, we identify the positions we want to place the expanded particle
+    // and its follower.
+    Node nodeTail(x, y);
     int partnerDir = randDir();
-    Node flwr = node.nodeInDir(partnerDir);
+    Node flwr = nodeTail.nodeInDir(partnerDir);
     int leaderExpandDir = randDir();
     while (leaderExpandDir == partnerDir) {
       leaderExpandDir = randDir();
     }
+    Node nodeHead = nodeTail.nodeInDir(leaderExpandDir);
 
-    // If unoccupied, place a particle there.
-    if (occupied.find(node) == occupied.end() &&
-        occupied.find(flwr) == occupied.end()) {
-      BallroomDemoParticle *one = new BallroomDemoParticle(node, leaderExpandDir, randDir(), *this,
+    // If unoccupied, place the particles there.
+    if ((occupied.find(nodeTail) == occupied.end() &&
+        occupied.find(flwr) == occupied.end()) &&
+        occupied.find(nodeHead) == occupied.end()) {
+      BallroomDemoParticle *one =
+          new BallroomDemoParticle(nodeTail, leaderExpandDir, randDir(), *this,
                                                   BallroomDemoParticle::State::Leader);
       insert(one);
-      BallroomDemoParticle *two = new BallroomDemoParticle(flwr, -1, randDir(), *this,
+
+      BallroomDemoParticle *two =
+          new BallroomDemoParticle(flwr, -1, randDir(), *this,
                                                   BallroomDemoParticle::State::Follower);
       insert(two);
+
+      // Then, we link the two particles by their 'neighbor' variable to keep
+      // track of their dance partner. This ensures the particles only dance
+      // with their original pairing.
       one->neighbor = &(*two);
       two->neighbor = &(*one);
-      occupied.insert(node);
+
+      // Here, we insert both the nodeHead and nodeTail positions so we
+      // do not insert particles into occupied positions.
+      occupied.insert(nodeTail);
       occupied.insert(flwr);
+      occupied.insert(nodeHead);
     }
   }
 }
