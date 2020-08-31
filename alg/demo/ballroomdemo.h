5dc60c2d218704714e866bf115cc32c581e60de5
Josh Daymude <joshdaymude@gmail.com>
2020-08-31 09:22:37 -0700

    ~ Used the shorted URL for ReadTheDocs and added URL in tutorial

--- alg/demo/ballroomdemo.h
+++ alg/demo/ballroomdemo.h
@@ -6,7 +6,7 @@
 // tutorial, demonstrating inter-particle coordination. This tutorial covers
 // read/write functionality and pull/push handovers. The pseudocode is
 // available in the docs:
-// [https://amoebotsim.readthedocs.io/en/latest/tutorials/tutorials.html#ballroomdemo-working-together].
+// [https://amoebotsim.rtfd.io/en/latest/tutorials/tutorials.html#ballroomdemo-working-together].
 
 #ifndef AMOEBOTSIM_ALG_DEMO_BALLROOMDEMO_H_
 #define AMOEBOTSIM_ALG_DEMO_BALLROOMDEMO_H_
